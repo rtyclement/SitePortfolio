635e2fa5765d85171eed874f3fb3799796c64143
Roty Clément <clementroty70@gmail.com>
2026-01-04 13:44:17 +0100

    refactor chemin img

--- js/main.js
+++ js/main.js
@@ -5,35 +5,35 @@ const projectsData = [
         description: "Pour ce projet, toujours mené par équipe, où nous étions 5, nous devions créer un jeu de labyrinthe en java avec javafx pour l'interface graphique en suivant les contraintes suivantes : Utilisation obligatoire du pattern Oberservable-Observer via son implémentation en MVC(modele-vue-controleur) en mode pull. La génération des labyrinthes est aléatoire, avec 2 algorithmes, l'un purement aléatoire à taux de mur, et l'autre qui est parfait, basé sur un parcours en DSF, avec une distance minimale entre entré et sortie.",
         skills: ["Java", "JavaFX", "Pattern MVC", "SOLID", "Clean Code"],
         link: "https://github.com/rtyclement/SAE_Labyrinthe",
-        images: ["../img/laby/img1.png","../img/laby/img2.png","../img/laby/img3.png","../img/laby/img4.png","../img/laby/img5.png","../img/laby/img6.png","../img/laby/img7.png","../img/laby/img8.png","../img/laby/img9.png","../img/laby/img10.png"]
+        images: ["img/laby/img1.png","img/laby/img2.png","img/laby/img3.png","img/laby/img4.png","img/laby/img5.png","img/laby/img6.png","img/laby/img7.png","img/laby/img8.png","img/laby/img9.png","img/laby/img10.png"]
     },
     {
         title: "Sirtet",
         description: "Découverte de l'environnement agile par groupe de 5. Nous devions inventer, créer et faire démonstration de notre projet. Nous avons donc en groupe développer une application en Java, qui avait comme contrainte d'avoir un affichage dans un terminal. Pour notre part, nous avons fait le choix de créer un jeu, nommé Sirtet, qui n'est qu'une réplique de Tetris.",
         skills: ["Java", "POO", "Lanterna", "Git", "Méthodologie Agile", "Sprint", "Backlog"],
         link: "https://github.com/rtyclement/SAE_Sirtet_Agile",
-        images: ["/img/sirtet/img0.png","/img/sirtet/img1.png","/img/sirtet/img2.png","/img/sirtet/img3.png"]
+        images: ["img/sirtet/img0.png","img/sirtet/img1.png","img/sirtet/img2.png","img/sirtet/img3.png"]
     },
     {
         title: "Application de Matching",
         description: "Le but de ce projet était de réaliser par groupe de 3, une application avec interface graphique qui devait répondre au besoin suivant : Permettre la gestion d'appariements entre étudiants dans le cadre d'un échange linguistique entre 4 pays différents. Notre application devait alors permettre l'importation d'un fichier CSV contenant une représentation d'une liste d'étudiants avec leurs contraintes et préférences, puis grâce à l'utilisation d'un graphe biparti, notre application effectuait un matching pour associer les étudiants entre eux.",
         skills: ["Java", "JavaFX", "POO", "JGraphT"],
         link: "https://github.com/rtyclement/SAE_Matching",
-        images: ["/img/matching/img1.png","/img/matching/img2.png","/img/matching/img3.png","/img/matching/img4.png","/img/matching/img5.png","/img/matching/img6.png"]
+        images: ["img/matching/img1.png","img/matching/img2.png","img/matching/img3.png","img/matching/img4.png","img/matching/img5.png","img/matching/img6.png"]
     },
     {
         title: "Plateforme Web de covoiturage",
         description: "Ce projet réalisé par groupe de 3, avait pour but de parvenir à la création d'un site internet proposant une solution de covoiturage. Nous devions nous inspirer du plan de mobilité de l'entreprise Pocheco pour développer une stratégie de mobilité éco-responsable. Le projet s'est segmenté en 3 grandes parties : élaboration d'une analyse stratégique de l'entreprise, réalisation du site web et création d'une newsletter promotionnelle.",
         skills:["HTML", "CSS", "UX/UI", "Travail en équipe"],
         link: "https://github.com/rtyclement/Site_Covoiturage",
-        images: ["/img/site/img1.png","/img/site/img2.png","/img/site/img3.png","/img/site/img4.png"]
+        images: ["img/site/img1.png","img/site/img2.png","img/site/img3.png","img/site/img4.png"]
     },
     {
         title: "BattleQuizDeluxe",
         description: "L'objectif de ce projet était de créer à partir de zéro un jeu ludo-pédagogique dans le langage IJava. Le jeu, BattleQuiz Deluxe, repose sur un système de combat au tour par tour où chaque action est validée par une question à choix multiple. Le projet incluait la conception du gameplay, la gestion des questions, l'implémentation complète du jeu ainsi qu'une soutenance orale et une démonstration auprès d'élèves de 6e.",
         skills: ["Algorithmique", "IJava", "Programmation séquentielle"],
         link: "https://github.com/rtyclement/Battle_Quiz_Deluxe",
-        images: ["/img/battlequiz/img0.png","/img/battlequiz/img1.png","/img/battlequiz/img2.png"]
+        images: ["img/battlequiz/img0.png","img/battlequiz/img1.png","img/battlequiz/img2.png"]
     }
 ];
 
@@ -272,4 +272,5 @@ document.querySelectorAll("nav a").forEach(link => {
     });
 
 });
+
 
